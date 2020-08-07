7379a1ce774e0376c3f1c1f7766a2e94ee284efb
whfuyn <whfuyn@outlook.com>
2020-08-07 19:01:57 +0800

    add a new error type

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,14 @@
-use raft::Error as RaftError;
-
 #[derive(Debug)]
 pub enum Error {
     RaftGroupUninitialized,
-    RaftInternal(RaftError),
+    RaftInternal(raft::Error),
     GrpcStatus(tonic::Status),
     GrpcTransport(tonic::transport::Error),
+    ProtoBuf(protobuf::error::ProtobufError),
 }
 
-impl From<RaftError> for Error {
-    fn from(error: RaftError) -> Self {
+impl From<raft::Error> for Error {
+    fn from(error: raft::Error) -> Self {
         Self::RaftInternal(error)
     }
 }
@@ -23,6 +22,12 @@ impl From<tonic::Status> for Error {
 impl From<tonic::transport::Error> for Error {
     fn from(error: tonic::transport::Error) -> Self {
         Self::GrpcTransport(error)
+    }
+}
+
+impl From<protobuf::error::ProtobufError> for Error {
+    fn from(error: protobuf::error::ProtobufError) -> Self {
+        Self::ProtoBuf(error)
     }
 }
 
