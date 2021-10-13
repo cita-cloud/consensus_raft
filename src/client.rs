21bc0716bec1452e1d68d506c15d4f3c0dfc0a5e
ypf <yuitta@163.com>
2021-10-13 14:04:41 +0800

    fix: notransaction cause error

--- src/client.rs
+++ src/client.rs
@@ -56,7 +56,7 @@ impl Controller {
 
         // horrible
         if let ProposalResponse {
-            status: Some(StatusCode { code: 0 }),
+            status: Some(StatusCode { code: 0 } | StatusCode { code: 109 }),
             proposal: Some(proposal),
         } = resp
         {
